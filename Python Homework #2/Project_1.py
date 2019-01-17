1d91346b2eee7e22d7b7d0c6fa30bbc8eccc1929
ad7608327 <46658259+ad7608327@users.noreply.github.com>
2019-01-17 07:51:55 +0200

    Add files via upload

---
+++ Python Homework #2/Project_1.py
@@ -0,0 +1,17 @@
+"""
+Home Work #2
+it is about Functions
+
+"""
+# Genre Artist Year
+def Genre():
+    A="Jazz"
+    return A
+def Artist():
+    B="Jim Hall"
+    return B
+def Year():
+    C="1962"
+    return C
+GEN, ART, YEAR = Genre(), Artist(), Year()
+print(GEN,'\n',ART,'\n',YEAR)
